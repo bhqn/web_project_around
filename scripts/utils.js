f30be57d871d2e56475661508132b9c9b20c5a97
Bruno Quintanilha <bernardo_quintanilha@hotmail.com>
2025-09-17 21:39:59 -0300

    new

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,7 +1,13 @@
 // ==========================
 // FUNÇÃO: Define os eventos de cada card (abrir imagem, deletar card)
 // ==========================
-export function setCardEventListeners(cardElement, link, name, api, imagePopup) {
+export function setCardEventListeners(
+  cardElement,
+  link,
+  name,
+  api,
+  imagePopup
+) {
   // Abrir popup ao clicar na imagem
   cardElement.querySelector(".gallery__image").addEventListener("click", () => {
     imagePopup.open({ link, name });
@@ -10,14 +16,36 @@ export function setCardEventListeners(cardElement, link, name, api, imagePopup) 
   // Remover card
   const removeButton = cardElement.querySelector(".card__button-remove");
   removeButton.addEventListener("click", () => {
-    api
-      .removeCard(cardElement.id)
-      .then(() => {
-        cardElement.remove();
-      })
-      .catch((err) => {
-        console.log("Erro ao remover card:", err);
-        // Aqui você pode adicionar uma mensagem visual de erro
-      });
+    const confirm = document.querySelector(".confirm");
+    confirm.style.display = "block";
+
+    const confirmBtn = confirmPopup.querySelector(".confirm__button");
+    const closeBtn = confirmPopup.querySelector(".confirm__button-close");
+
+    const handleConfirm = () => {
+      api
+        .removeCard(cardElement.id)
+        .then(() => {
+          cardElement.remove();
+        })
+        .catch((err) => {
+          console.log("Erro ao remover card:", err);
+          // Aqui você pode adicionar uma mensagem visual de erro
+        });
+
+      confirm.style.display = "none";
+    };
+
+    const handleClose = () => {
+      confirmPopup.style.display = "none";
+      cleanup();
+    };
+
+    const cleanup = () => {
+      confirmBtn.removeEventListener("click", handleConfirm);
+      closeBtn.removeEventListener("click", handleClose);
+    };
+    confirmBtn.addEventListener("click", handleConfirm);
+    closeBtn.addEventListener("click", handleClose);
   });
 }
